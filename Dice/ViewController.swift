fe38672fe3f2197c895ba5f3295f63bdb9ecb8be
David Heath <heathdj@gmail.com>
2021-04-19 17:54:13 -0700

    Basic Code for rolling 2 D6 dice.

--- Dice/ViewController.swift
+++ Dice/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    // Reference to left dice image in storyboard.
+    @IBOutlet weak var leftDiceImage: UIImageView!
+    
+    // Reference to right dice image in storyboard.
+    @IBOutlet weak var rightDiceImage: UIImageView!
+    
+    // Array of all D6 face images
+    let D6 = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo") , #imageLiteral(resourceName: "DiceThree") , #imageLiteral(resourceName: "DiceFour")  , #imageLiteral(resourceName: "DiceFive") , #imageLiteral(resourceName: "DiceSix")]
+    
+    
+    // Action for Button Clicked
+    @IBAction func rollBtnClicked(_ sender: UIButton) {
+        leftDiceImage.image = D6.randomElement()!
+        rightDiceImage.image = D6.randomElement()!
     }
-
-
 }
 
